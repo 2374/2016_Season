c92cd8a748ea5b61f92ed7db43e2be4f6e721a72
robotics <crusaderbots@gmail.com>
2016-01-29 19:28:56 -0800

    Autonomous revisions

--- Robot_2016/src/org/usfirst/frc/team2374/robot/controllers/AutonomousController.java
+++ Robot_2016/src/org/usfirst/frc/team2374/robot/controllers/AutonomousController.java
@@ -8,95 +8,84 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class AutonomousController extends RobotController {
-	
+
 	protected int autoCase;
-	
-    public AutonomousController(Robot robot) {
-        super(robot);
-    }
-    
-    /*
-     * FOCUS ON ROUGH TERRAIN AND MOAT FOR AUTONOMOUS
-     */
-    
-    
-   //private SendableChooser autoChooser = new SendableChooser(); //Add defaults and options for rough terrain and various other obstacles
-    
-    @Override
-    protected void onStart() {
-    	autoCase=(int) myRobot.autoChooser.getSelected();
-    	switch(autoCase){
-    	case 1: //ROUGH TERRAIN AUTONOMOUS
-    		myRobot.drivetrain.setSolenoids(0);
-    		myRobot.drivetrain.setSpeed(.5,.5);
-    		delay(2, () -> {
-    			myRobot.drivetrain.setSpeed(0, 0);;
-    		});
-    	break;
-    	case 2: //MOAT AUTONOMOUS
-    		myRobot.drivetrain.setSolenoids(0);
-    		myRobot.drivetrain.setSpeed(.5,.5);
-    		delay(2, () -> {
-    			myRobot.drivetrain.setSpeed(0,0);
-    		});
-    		break;
-    		
-    	}
-        //Move forward full speed
-        myRobot.drivetrain.setSpeed(1, 1);
-        //Wait 2 seconds, delay duration subject to change
-        delay(2, () -> {
-            //Stop moving
-            myRobot.drivetrain.setSpeed(0, 0);
-        });
-        
-    }
 
-    @Override
-    protected void onUpdate() {
-    }
+	public AutonomousController(Robot robot) {
+		super(robot);
+	}
 
-    @Override
-    protected void onFinish() {
-    }
+	/*
+	 * FOCUS ON ROUGH TERRAIN AND MOAT FOR AUTONOMOUS
+	 */
 
-    @Override
-    protected boolean shouldFinish() {
-        return myRobot.isDisabled() || !myRobot.isAutonomous();
-    }
+	// private SendableChooser autoChooser = new SendableChooser(); //Add
+	// defaults and options for rough terrain and various other obstacles
 
-    //Old autonomous code
-    /*
-    public void worstCaseAutonomous() {
-        myRobot.setSafetyEnabled(false);
-        drivetrain.update(1, 1, false, false);
-        Timer.delay(2.0);//delay duration subject to change
-        drivetrain.update(0, 0, false, false);//move forward full speed 2 seconds
-    }
+	@Override
+	protected void onStart() {
+		autoCase = (int) myRobot.autoChooser.getSelected();
+		switch (autoCase) {
+		case 1: // ROUGH TERRAIN AUTONOMOUS
+			myRobot.drivetrain.setSolenoids(0);
+			myRobot.drivetrain.setSpeed(.5, .5);
+			delay(2, () -> {
+				myRobot.drivetrain.setSpeed(0, 0);
+				;
+			});
+			break;
+		case 2: // MOAT AUTONOMOUS
+			myRobot.drivetrain.setSolenoids(0);
+			myRobot.drivetrain.setSpeed(.5, .5);
+			delay(2, () -> {
+				myRobot.drivetrain.setSpeed(0, 0);
+			});
+			break;
 
-    public void terrainAutonomous() {//make more terrain autonomous modes
-        myRobot.setSafetyEnabled(false);
-        gyro.initGyro();
-        drivetrain.update(1, 1, true, false);
-        Timer.delay(4.0);//delay duration subject to change
-        if (gyro.getRate() <= 1 && gyro.getRate() >= -1) {//values subject to change in future
-            drivetrain.update(1, 1, true, false);
-            Timer.delay(2.0);//delay duration subject to change
-        }
-        drivetrain.update(0, 0, false, false);
-    }
+		}
+		// Move forward full speed
+		myRobot.drivetrain.setSpeed(1, 1);
+		// Wait 2 seconds, delay duration subject to change
+		delay(2, () -> {
+			// Stop moving
+				myRobot.drivetrain.setSpeed(0, 0);
+			});
 
-    public void terrainAndShootAutonomous() {
-        myRobot.setSafetyEnabled(false);
-        gyro.initGyro();
-        drivetrain.update(1, 1, true, false);
-        Timer.delay(4.0);//delay duration subject to change
-        if (gyro.getRate() <= 1 && gyro.getRate() >= -1) {//values subject to change as well
-            drivetrain.update(1, 1, true, false);
-            Timer.delay(2.0);//delay duration subject to change
-        }
-        drivetrain.update(0, 0, false, false);
-        angledShooter.update(1, true, false);
-    }
-     */
+	}
+
+	@Override
+	protected void onUpdate() {
+	}
+
+	@Override
+	protected void onFinish() {
+	}
+
+	@Override
+	protected boolean shouldFinish() {
+		return myRobot.isDisabled() || !myRobot.isAutonomous();
+	}
+
+	// Old autonomous code
+	/*
+	 * public void worstCaseAutonomous() { myRobot.setSafetyEnabled(false);
+	 * drivetrain.update(1, 1, false, false); Timer.delay(2.0);//delay duration
+	 * subject to change drivetrain.update(0, 0, false, false);//move forward
+	 * full speed 2 seconds }
+	 * 
+	 * public void terrainAutonomous() {//make more terrain autonomous modes
+	 * myRobot.setSafetyEnabled(false); gyro.initGyro(); drivetrain.update(1, 1,
+	 * true, false); Timer.delay(4.0);//delay duration subject to change if
+	 * (gyro.getRate() <= 1 && gyro.getRate() >= -1) {//values subject to change
+	 * in future drivetrain.update(1, 1, true, false); Timer.delay(2.0);//delay
+	 * duration subject to change } drivetrain.update(0, 0, false, false); }
+	 * 
+	 * public void terrainAndShootAutonomous() {
+	 * myRobot.setSafetyEnabled(false); gyro.initGyro(); drivetrain.update(1, 1,
+	 * true, false); Timer.delay(4.0);//delay duration subject to change if
+	 * (gyro.getRate() <= 1 && gyro.getRate() >= -1) {//values subject to change
+	 * as well drivetrain.update(1, 1, true, false); Timer.delay(2.0);//delay
+	 * duration subject to change } drivetrain.update(0, 0, false, false);
+	 * angledShooter.update(1, true, false); }
+	 */
 }
