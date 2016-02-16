3f963ca98f5b1cf8232aa229a4f629902c70328b
Bhargav and Isaac <bannigeri17@jesuitmail.org>
2016-02-16 13:48:18 -0800

    Added command for pistons

--- Robot_2016/src/org/usfirst/frc/team2374/robot/controllers/TeleopController.java
+++ Robot_2016/src/org/usfirst/frc/team2374/robot/controllers/TeleopController.java
@@ -2,6 +2,7 @@ package org.usfirst.frc.team2374.robot.controllers;
 
 import org.usfirst.frc.team2374.robot.Controller;
 import org.usfirst.frc.team2374.robot.commands.ForwardsCommand;
+import org.usfirst.frc.team2374.robot.commands.PistonsCommand;
 import org.usfirst.frc.team2374.robot.commands.TankDriveCommand;
 import org.usfirst.frc.team2374.robot.events.Input;
 
@@ -11,6 +12,9 @@ public class TeleopController extends Controller {
     public void start() {
         //This controls the robot's wheels
         new TankDriveCommand().startAsDefaultCommand();
+        
+        //controls the pistons
+        new PistonsCommand().startAsDefaultCommand();
 
         //Make the robot go forward for 1 second on the A button
         Input.whenPressed(1).runCommand(new ForwardsCommand(1));
