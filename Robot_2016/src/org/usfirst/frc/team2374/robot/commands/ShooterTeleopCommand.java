91c9ecddb893c9b0a83dfa6d41e0dadab38ec1c8
robotics <robotics@S-ROBOTICS02.jesuitpdx.org>
2016-02-19 16:19:35 -0800

    Fixed second controller capabilities

--- Robot_2016/src/org/usfirst/frc/team2374/robot/commands/ShooterTeleopCommand.java
+++ Robot_2016/src/org/usfirst/frc/team2374/robot/commands/ShooterTeleopCommand.java
@@ -8,33 +8,41 @@ import org.usfirst.frc.team2374.robot.Component;
 import org.usfirst.frc.team2374.robot.Robot;
 import org.usfirst.frc.team2374.robot.events.Input;
 
-public class ShooterTeleopCommand extends Command{
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class ShooterTeleopCommand extends Command {
 	@Override
-    public boolean isFinished() {
-        return false;
+	public boolean isFinished() {
+		return false;
 	}
-   public boolean FirstControllerActive(){
-		 return (Input.JOYSTICK1.getRawButton(5)) || (Input.JOYSTICK1.getRawButton(6));
-    }
+
+	public boolean FirstControllerActive() {
+		return (Input.JOYSTICK1.getRawButton(5))
+				|| (Input.JOYSTICK1.getRawButton(6));
+	}
 
 	@Override
-    public List<Component> requires() {
-        return Arrays.asList(Robot.shooter);
-    }
-  
+	public List<Component> requires() {
+		return Arrays.asList(Robot.shooter);
+	}
+
 	@Override
- 	public void update () {
-	 if (Input.JOYSTICK1.getRawButton(5)){
-		 Robot.shooter.setSpeed(1);//implement PID
-	 }
-	 if (Input.JOYSTICK1.getRawButton(6)){
-		 Robot.shooter.setSpeed(-1);//implement PID
-	 }
-	 if (!FirstControllerActive() && Input.JOYSTICK2.getRawAxis(2)!=0){
-		 Robot.shooter.setSpeed(1);//implement PID
-	 }
-	 if (!FirstControllerActive() && Input.JOYSTICK2.getRawAxis(3)!=0){
-		 Robot.shooter.setSpeed(-1);//implement PID
-	 }
+	public void update() {
+		if (Input.JOYSTICK1.getRawButton(5)) {
+			Robot.shooter.setSpeed(1);// implement PID
+		} else if (Input.JOYSTICK1.getRawButton(6)) {
+			Robot.shooter.setSpeed(-1);// implement PID
+		} else if (!FirstControllerActive() && Input.JOYSTICK2.getRawButton(5)) {
+			Robot.shooter.setSpeed(1);// implement PID
+			SmartDashboard.putString("controller 2 Raw Button 5",
+					"Shooter Active-Forwards");
+		} else if (!FirstControllerActive() && Input.JOYSTICK2.getRawButton(6)) {
+			Robot.shooter.setSpeed(-1);// implement PID
+			SmartDashboard.putString("controller 2 Raw Button 6",
+					"Shooter Active-Reverse");
+		} else {
+			Robot.shooter.setSpeed(0);
+			SmartDashboard.putString("Should shooter be active?", "NO");
+		}
 	}
 }
